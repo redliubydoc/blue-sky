ddb242ea92b362b045f7451ddda46cd902c7346a
CodyBuilder <rajeswar.sarkar.2009@gmail.com>
2021-07-01 00:53:45 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,50 @@
+import requests
+import os
+
+from flask import Flask, render_template, request, send_from_directory
+from credentials import appid
+
+if appid == '':
+    appid = os.environ.get('TERM')
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/img/<file>')
+def imgs(file):
+    return send_from_directory('static/img', path=file)
+
+@app.route('/api')
+def api():
+    lat = request.args.get('lat')
+    lon = request.args.get('lon')
+    q = request.args.get('q')    
+    
+    if lat is not None and lon is not None:
+        params = {
+            'appid': appid,
+            'lat': lat,
+            'lon': lon,
+            'units': 'metric',
+            'lang': 'us'
+        }
+        response = requests.get('https://api.openweathermap.org/data/2.5/weather', params=params)
+        response.close()
+        return response.json()
+
+    elif q is not None:
+        params = {
+            'appid': appid,
+            'q': q,
+            'units': 'metric',
+            'lang': 'us'
+        }
+        response = requests.get('https://api.openweathermap.org/data/2.5/weather', params=params)
+        response.close()
+        return response.json()
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/main.js
@@ -0,0 +1,114 @@
+const protocol = 'http';
+const host = window.location.host;
+
+const placeholderCity = document.getElementById('main-city');
+const placeholderTemp = document.getElementById('main-temp');
+const placeholderIcon = document.getElementById('main-icon');
+// const placeholderMin = document.getElementById('main-min');
+// const placeholderMax = document.getElementById('main-max');
+const placeholderCondition = document.getElementById('main-condition');
+const placeholderHumidity = document.getElementById('main-humidity');
+const placeholderPressure = document.getElementById('main-pressure');
+const placeholderWind = document.getElementById('main-wind');
+const placeholderFeelslike = document.getElementById('main-feelslike');
+const inputSearch = document.getElementById('main-search-input');
+const buttonSearch = document.getElementById('main-search-button');
+
+
+let wthData;
+let currentLat;
+let currentLon;
+
+
+// to fetch weather data based on current location
+(function getWthDataByGeo() {
+    
+    // fetch current location
+    let xhr1 = new XMLHttpRequest();
+
+    xhr1.open('get', 'http://ip-api.com/json/', false);
+
+    xhr1.onload = function() {
+        if(this.status === 200) {
+            let response = JSON.parse(this.responseText);
+            currentLat = response.lat;
+            currentLon = response.lon;
+        }
+    }
+
+    xhr1.send();
+
+    // get weather data based on current location
+    let xhr2 = new XMLHttpRequest();
+    xhr2.open('get', `${protocol}://${host}/api?lat=${currentLat}&lon=${currentLon}`, true);
+
+    xhr2.onload = function() {
+        if(this.status === 200) {
+            wthData = JSON.parse(this.responseText);
+            
+            // on receiving valid data
+            if(wthData.cod === 200) {
+                show();
+            }
+        }
+    }
+
+    xhr2.send();
+})();
+
+function show() {
+    
+    placeholderCity.innerText = wthData.name;
+    placeholderTemp.innerText = `${Math.round(wthData.main.temp)}\xB0C`;
+    placeholderIcon.src = `http://openweathermap.org/img/wn/${wthData.weather[0].icon}@4x.png`;
+    placeholderCondition.innerText = wthData.weather[0].description; 
+    placeholderHumidity.innerText = `${Math.round(wthData.main.humidity)} %`;
+    placeholderPressure.innerText = `${Math.round(wthData.main.pressure)} hPa`;
+    placeholderWind.innerText = `${Math.round(wthData.wind.speed * 36)/10 } kph`;
+    placeholderFeelslike.innerText = `${Math.round(wthData.main.feels_like)}\xB0C`;
+
+
+    // day or night 
+    let currentTime = (new Date).getTime() / 1000;
+    if(currentTime >= wthData.sys.sunrise && currentTime <= wthData.sys.sunset) {
+        document.getElementById('main-container').style.cssText = `
+            background-image: url('${protocol}://${host}/img/bg_day.png');
+        `;
+    }
+    else {
+        document.getElementById('main-container').style.cssText = `
+            background-image: url('${protocol}://${host}/img/bg_night.png');
+        `;
+    }
+}
+
+buttonSearch.addEventListener('click', () => {
+
+    if(inputSearch.value === undefined || inputSearch.value === null || inputSearch.value == "") {
+        window.alert('Empty Field!');
+        return;
+    }
+    
+    buttonSearch.className = 'btn btn-success';
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('get', `${protocol}://${host}/api?q=${inputSearch.value}`, true);
+
+    xhr.onload = function() {
+        if(this.status === 200) {
+            wthData = JSON.parse(this.responseText);
+            
+            // on receiving valid data
+            if(wthData.cod === 200) {
+                show();
+            }
+            else {
+                window.alert('Invalid Search Query!');
+            }
+            buttonSearch.className = 'btn btn-danger';
+            inputSearch.value = '';
+        }
+    }
+
+    xhr.send();
+});
